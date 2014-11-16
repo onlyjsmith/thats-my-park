c77817fb0dac8a0822bb4188efb13df7e0ad2ba2
Jonathan Smith <jonathan@peoplesized.com>
2014-11-16 12:42:10 +0000

    this is the design you're getting

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -2,4 +2,16 @@ class FarmsController < ApplicationController
   def new
     @farm = Farm.new
   end
+
+  def start 
+  end
+
+  def track
+  end
+
+  def submit
+  end
+
+  def submitted
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :farms
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +6,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'farms#new'
 
+   get 'farms/start' => 'farms#start'
+   get 'farms/track' => 'farms#track'
+   get 'farms/submit' => 'farms#submit'
+   get 'farms/submitted' => 'farms#submitted'
+
+  resources :farms
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
